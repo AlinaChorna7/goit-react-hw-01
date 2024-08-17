49fc9c7de2c180e16f1a1f8e49b5891ce469c801
AlinaChorna7 <159713721+AlinaChorna7@users.noreply.github.com>
2024-08-17 12:43:00 +0200

    add initial files

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,14 +1,9 @@
 import styles from "./FriendList.module.css"
+import FriendListItem from '../FriendListItem/FriendListItem';
 
-const FriendListItem = ({avatar, name,isOnline})=>{
-    return(
-        <div className={styles.card}>
-           <img src={avatar} alt={`${name} avatar`} width="48"/>
-           <p className={styles.name}>{name}</p>
-           <p className={isOnline ? styles.online : styles.offline}>{isOnline ? 'Online' : 'Offline'}</p>
-        </div>
-    )
-}
+
+
+
 const FriendList= ({friends})=>{
 return(<ul className={styles.list}>
 
